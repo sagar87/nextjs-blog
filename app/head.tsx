0fec941260cd47bd0f34d31560e8a73415221b02
Harald Vohringer <harald.voeh@gmail.com>
2025-07-04 15:44:41 +0200

    fix favicon.ico issue

--- app/head.tsx
+++ app/head.tsx
@@ -5,7 +5,7 @@ const Head = () => {
         <title>Haralds blog</title>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="favicon.ico"></link>
+        <link rel="icon" href="/favicon.ico"></link>
       </head>
     </>
   );
